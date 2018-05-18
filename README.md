fcb256f59f7e6478db38caf4d776a71c6c06088f
bingo8670 <307286130@qq.com>
2018-05-18 16:37:26 +0800

    hello.go

---
+++ README.md
@@ -0,0 +1,4 @@
+## Go by Example
+### 命令集
+- go run xxx.go 执行xxx脚本；
+- go build xxx.go 将xxx脚本转换为二进制文件；输入文件路径即可执行；
---
+++ hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("Hello, world.\n")
+}
+// go build hello.go 将hello脚本转换为二进制文件；输入文件路径 ./hello-world 即可执行；
