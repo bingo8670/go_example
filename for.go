48150034ce01a954d9ea5e39c219264e7601ee81
bingo8670 <307286130@qq.com>
2018-05-21 09:51:59 +0800

    5.21 daka

---
+++ for.go
@@ -0,0 +1,43 @@
+package main
+import "fmt"
+func main() {
+    i := 1
+
+// for是Go唯一的循环构造。
+
+    for i <= 3 {
+        fmt.Println(i)
+        i = i + 1
+    }
+
+    for j := 7; j <= 9; j++ {
+        fmt.Println(j)
+    }
+
+    for {
+        fmt.Println("loop")
+        break
+        // break 方法用于停止该无限循环；
+    }
+
+    for n := 0; n <= 5; n++ {
+        if n%2 == 0 {
+            continue
+            // continue 方法用于跳过符合条件的情况；
+        }
+        fmt.Println(n)
+    }
+}
+
+
+// $ go run for.go
+// 1
+// 2
+// 3
+// 7
+// 8
+// 9
+// loop
+// 1
+// 3
+// 5
--- hello.go
+++ hello.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world.\n")
 }
+
+// $ go run hello-world.go
+// hello world
+// $ go build hello-world.go
+// $ ./hello-world
+// hello world
+
 // go build hello.go 将hello脚本转换为二进制文件；输入文件路径 ./hello-world 即可执行；
