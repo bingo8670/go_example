3cd866f55ecc3919da03e5d84fe83090947c36b0
bingo8670 <307286130@qq.com>
2018-05-25 23:53:10 +0800

    5.25 daka

--- 29-non-blocking-channel-operations.go
+++ 29-non-blocking-channel-operations.go
@@ -1,10 +1,11 @@
-// 通道上的基本发送和接收处于阻塞状态。但是，我们可以使用带有默认子句的select来实现非阻塞式发送，接收，甚至是非阻塞式多路选择。
+// 常规的通过通道发送和接收数据是阻塞的。然而，我们可以使用带一个 default 子句的 select 来实现非阻塞 的发送、接收，甚至是非阻塞的多路 select。
 package main
 import "fmt"
 func main() {
     messages := make(chan string)
     signals := make(chan bool)
 
+// 这里是一个非阻塞接收的例子。如果在 messages 中存在，然后 select 将这个值带入 <-messages case中。如果不是，就直接到 default 分支中。
     select {
     case msg := <-messages:
         fmt.Println("received message", msg)
@@ -20,6 +21,7 @@ func main() {
         fmt.Println("no message sent")
     }
 
+// 我们可以在 default 前使用多个 case 子句来实现一个多路的非阻塞的选择器。这里我们试图在 messages和 signals 上同时使用非阻塞的接受操作。
     select {
     case msg := <-messages:
         fmt.Println("received message", msg)
--- 32-timers.go
+++ 32-timers.go
@@ -1,14 +1,17 @@
-// 我们经常希望在将来某个时候执行Go代码，或者在某个时间间隔内重复执行Go代码。 Go的内置计时器和自动收报机功能使这两项任务变得简单。我们先看看定时器，然后看看代码。
+// 我们常常需要在后面一个时刻运行 Go 代码，或者在某段时间间隔内重复运行。Go 的内置 定时器 和 打点器 特性让这些很容易实现。我们将先学习定时器，然后再学习打点器。
 package main
 import "time"
 import "fmt"
 func main() {
 
+// 定时器表示在未来某一时刻的独立事件。你告诉定时器需要等待的时间，然后它将提供一个用于通知的通道。这里的定时器将等待 2 秒。
     timer1 := time.NewTimer(2 * time.Second)
 
+// <-timer1.C 直到这个定时器的通道 C 明确的发送了定时器失效的值之前，将一直阻塞。
     <-timer1.C
     fmt.Println("Timer 1 expired")
 
+// 如果你需要的仅仅是单纯的等待，你需要使用 time.Sleep。定时器是有用原因之一就是你可以在定时器失效之前，取消这个定时器。这是一个例子
     timer2 := time.NewTimer(time.Second)
     go func() {
         <-timer2.C
@@ -19,6 +22,8 @@ func main() {
         fmt.Println("Timer 2 stopped")
     }
 }
+
+// 第一个定时器将在程序开始后 ~2s 失效，但是第二个在它没失效之前就停止了。
 // $ go run 32-timers.go
 // Timer 1 expired
 // Timer 2 stopped
